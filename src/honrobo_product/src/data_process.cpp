cf293e3ee3a619761e258a243e1d4c2813451ca4
naga <toritonsuibu0527@gmail.com>
2022-09-13 00:33:44 +0900

    serial communication node

--- src/honrobo_product/src/data_process.cpp
+++ src/honrobo_product/src/data_process.cpp
@@ -10,32 +10,32 @@
  * 
  */
 #include <ros/ros.h>
-#include <std_msgs/Int32MultiArray.h>
+#include <std_msgs/Int8MultiArray.h>
 #include <my_msgs/can.h>
 
 my_msgs::can can_msg;
 void can_rx_callback(const my_msgs::can& CAN_RX){
-    can_msg = CAN_RX;
+	can_msg = CAN_RX;
 }
 
 
-void controller_callback(const std_msgs::Int32MultiArray &f){
+void controller_callback(const std_msgs::Int8MultiArray &f){
     
 }
 
 int main(int argc, char**argv){
-    ros::init(argc, argv, "data_process");
-    ros::NodeHandle nh;
-    ros::Publisher can_tx = nh.advertise<my_msgs::can>("CAN_TX", 10);
-    ros::Subscriber can_rx = nh.subscribe("CAN_RX", 10, can_rx_callback);
-    ros::Subscriber controller_input = nh.subscribe("control_data", 10, controller_callback);
+	ros::init(argc, argv, "data_process");
+	ros::NodeHandle nh;
+	ros::Publisher can_tx = nh.advertise<my_msgs::can>("CAN_TX", 10);
+	ros::Subscriber can_rx = nh.subscribe("CAN_RX", 10, can_rx_callback);
+	ros::Subscriber controller_input = nh.subscribe("control_data", 10, controller_callback);
 	// need more?
 
-    ros::Rate rate(10);
+	ros::Rate rate(10);
 
-    while(ros::ok()){
-        //TODO
-        
-        rate.sleep();
-    }
+	while(ros::ok()){
+		//TODO
+		
+		rate.sleep();
+	}
 }
---
+++ src/honrobo_product/src/wireless.cpp
@@ -0,0 +1,121 @@
+/**
+ * @file wireless.cpp
+ * @author naga
+ * @brief 
+ * @version 0.1
+ * @date 2022-09-12
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+#include <ros/ros.h>
+#include <libserial/SerialStream.h>
+
+#include <std_msgs/MultiArrayDimension.h>
+#include <std_msgs/MultiArrayLayout.h>
+#include <std_msgs/UInt8MultiArray.h>
+
+using namespace std;
+using namespace LibSerial;
+
+void serial_set(SerialStream& serial);
+bool read(SerialStream& serial, vector<uint8_t>& send_data);
+bool check_start(SerialStream&serial);
+bool check_sum(SerialStream& serial, char data[]);
+
+
+int main(int argc, char** argv){
+	ros::init(argc, argv, "wireless");
+	ros::NodeHandle nh;
+	ros::Publisher pub = nh.advertise<std_msgs::UInt8MultiArray>("control_data", 10);
+
+	std_msgs::UInt8MultiArray msg;
+	msg.data.clear();
+	
+	SerialStream serial;
+	serial_set(serial);
+
+	ros::Rate rate(10);
+
+	while(ros::ok()){
+		vector<uint8_t> send_data(9);
+		if(!read(serial, send_data)) continue;
+		for(auto i = 0lu; i < send_data.size(); i++){
+			msg.data.push_back(send_data[i]);
+		}
+		rate.sleep();
+	}
+
+}
+
+
+
+void serial_set(SerialStream& serial){
+	serial.Open("/dev/ttyUSB0");
+	serial.SetBaudRate(BaudRate::BAUD_115200);
+	serial.SetCharacterSize(CharacterSize::CHAR_SIZE_8);
+	serial.SetFlowControl(FlowControl::FLOW_CONTROL_NONE);
+	serial.SetParity(Parity::PARITY_NONE);
+	serial.SetStopBits(StopBits::STOP_BITS_1);
+}
+
+bool read(SerialStream& serial, vector<uint8_t>& send_data){
+	char str[8];
+
+	if(check_start(serial)){
+		serial.read(str, 8);
+	} else {
+		ROS_WARN("serial could not get header data");
+		return false;
+	}
+
+	if(!check_sum(serial, str)){
+		ROS_WARN("check sum is not good");
+		return false;
+	}
+
+	for(auto i = 0LU; i < send_data.size(); i++){
+		send_data[i] = str[i];
+	}
+	return true;
+}
+
+bool check_start(SerialStream& serial){
+	char check_start = 0;
+
+	constexpr char start_byte[] = "aa";//need to decide value
+
+	while(serial.good()){
+		serial>>check_start;
+		if(check_start == start_byte[0]){
+			serial>>check_start;
+			if(check_start == start_byte[1]){
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+bool check_sum(SerialStream& serial, char data[]){
+	constexpr char start_byte[] = "aa";//need to decide value
+
+	string str(data);
+	uint8_t sum = 0, recv = 0;
+	
+	for(int i = 0LU; i < strlen(start_byte); i++){
+		sum += start_byte[i];
+	}
+
+	for(auto i: str){
+		sum += i;
+	}
+
+	serial>>recv;
+
+	if(recv == sum) return true;
+	return false;
+}
