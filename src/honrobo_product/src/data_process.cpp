c7cab2d3672b9abd3453b62b83affbf4c3662f3d
naga <toritonsuibu0527@gmail.com>
2022-09-15 00:23:15 +0900

    write receive part from UART

--- src/honrobo_product/src/data_process.cpp
+++ src/honrobo_product/src/data_process.cpp
@@ -10,32 +10,275 @@
  * 
  */
 #include <ros/ros.h>
-#include <std_msgs/Int8MultiArray.h>
-#include <my_msgs/can.h>
+#include <std_msgs/UInt8MultiArray.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <my_msgs/can_msg.h>
 
-my_msgs::can can_msg;
-void can_rx_callback(const my_msgs::can& CAN_RX){
-	can_msg = CAN_RX;
+#define TEST // need to remove
+
+#ifdef TEST
+#define TEST 1
+#endif //TEST
+
+#ifdef PRODUCT
+#define PRODUCT 1
+#endif // PRODUCT
+
+bool is_emergency = false;
+
+// structures
+struct input_status{
+	bool neatly = false,
+		 sense_cycle_update = false,
+		 tire_return        = false,
+		 robot_return       = false,
+		 shot_and_reload    = false,
+		 change_cartridge   = false,
+		 return_to_start    = false,
+		 shot_cycle_update  = false;
+
+	uint8_t spin = 0,
+			shot_angle = 0,
+			x_velo = 0,
+			y_velo = 0;
+};
+
+class cycle{
+	uint8_t cycle_val = 0;
+
+public:
+	enum MODE{
+		HI_SENSE,
+		MIDDLE_SENSE,
+		LOW_SENSE
+	};
+
+	cycle(){}
+	~cycle(){}
+
+	void operator++(){
+		cycle_val++;
+		cycle_val = (cycle_val == 3)? 0: cycle_val;
+	}
+
+	uint8_t get_cycle_state(){ return cycle_val; }
+
+};
+
+//prototype decl
+uint32_t float_to_int(float f);
+void convert_32_8(uint32_t i, uint8_t*buf);
+
+//callback functions
+my_msgs::can_msg can_msg;
+void can_rx_callback(const my_msgs::can_msg& CAN_RX){
+	can_msg.id = CAN_RX.id;
+			
+#if TEST
+	ROS_INFO("receive data: %d, %d, %d, %d, %d, %d, %d, %d", can_msg.data[0], can_msg.data[1], can_msg.data[2], can_msg.data[3], can_msg.data[4], can_msg.data[5], can_msg.data[6], can_msg.data[7]);
+#endif // TEST
+
+#if PRODUCT
+
+#endif // PRODUCT
 }
 
-
-void controller_callback(const std_msgs::Int8MultiArray &f){
-    
+bool is_new_value = false;
+std_msgs::UInt8MultiArray rx_array;
+void controller_callback(const std_msgs::UInt8MultiArray &f){
+	for(auto i = 0lu; i < rx_array.data.size(); i++){
+		rx_array.data[i] = f.data[i];
+	}
+	is_new_value = true;
 }
+
+#if TEST
+
+#endif //TEST
 
 int main(int argc, char**argv){
 	ros::init(argc, argv, "data_process");
 	ros::NodeHandle nh;
-	ros::Publisher can_tx = nh.advertise<my_msgs::can>("CAN_TX", 10);
+	ros::Publisher can_tx = nh.advertise<my_msgs::can_msg>("CAN_TX", 10);
 	ros::Subscriber can_rx = nh.subscribe("CAN_RX", 10, can_rx_callback);
+	ros::Publisher controller_output = nh.advertise<std_msgs::Float32MultiArray>("lidar data", 10);
 	ros::Subscriber controller_input = nh.subscribe("control_data", 10, controller_callback);
 	// need more?
+
+	cycle sense;
+	cycle shot_spd;
 
 	ros::Rate rate(10);
 
 	while(ros::ok()){
+		input_status state;
+		bool is_update_value;
+
+		if(is_new_value){
+			state.neatly             = can_msg.data[0] & 0b10000000;
+			state.sense_cycle_update = can_msg.data[0] & 0b01000000;
+			state.tire_return        = can_msg.data[0] & 0b00100000;
+			state.robot_return       = can_msg.data[0] & 0b00010000;
+			state.shot_and_reload    = can_msg.data[0] & 0b00001000;
+			state.change_cartridge   = can_msg.data[0] & 0b00000100;
+			
+			state.return_to_start    = can_msg.data[1] & 0x00000100;
+
+			state.shot_cycle_update  = can_msg.data[6]>50? true: false;
+
+			state.spin       = can_msg.data[2];
+			state.shot_angle = can_msg.data[3];
+			state.x_velo     = can_msg.data[4];
+			state.y_velo     = can_msg.data[5];
+			
+
+			is_new_value = false;
+			is_update_value = true;
+		}
+
+		if(state.neatly){
+			//TODO
+		}
+
+		if(state.sense_cycle_update){
+			++sense;
+		}
+
+		if(state.tire_return){
+			my_msgs::can_msg msg;
+			msg.id = 0x12;
+			can_tx.publish(msg);
+		}
+
+		if(state.robot_return){
+			my_msgs::can_msg msg;
+			msg.id = 0x13;
+			can_tx.publish(msg);
+		}
+
+		if(state.shot_and_reload){
+			my_msgs::can_msg msg_0x2n;
+			my_msgs::can_msg msg_0x5n;
+
+			msg_0x2n.id = 0x20;
+			msg_0x5n.id = 0x50;
+		}
+
+		if(state.change_cartridge){
+			my_msgs::can_msg msg_0x5n;
+			my_msgs::can_msg msg_0x6n;
+
+			msg_0x5n.id = 0x51;
+			msg_0x6n.id = 0x60;
+			can_tx.publish(msg_0x5n);
+			can_tx.publish(msg_0x6n);
+		}
+
+		if(state.shot_cycle_update){
+			++shot_spd;
+
+			my_msgs::can_msg msg;
+			msg.id = 0x21;
+			msg.data; //have to decide protocol
+		}
+
+		if(is_update_value){
+			///@brief sense coeff 
+			///       have to change to controller's opinion
+			constexpr float hi_sense_coeff     = 3/128,
+							middle_sense_coeff = 1/128,
+							low_sense_coeff    = 0.5/128;
+			constexpr float spin_hi_sense_coeff     = 2*M_PI/128,
+							spin_middle_sense_coeff = 0.5*M_PI/128,
+							spin_low_sense_coeff    = 0.25*M_PI/128;
+
+			float pub_x = 0, pub_y = 0, pub_spin = 0;
+
+			switch (sense.get_cycle_state()){
+			case sense.HI_SENSE:
+				pub_x = state.x_velo * hi_sense_coeff;
+				pub_y = state.y_velo * hi_sense_coeff;
+				pub_spin = state.spin * spin_hi_sense_coeff;
+				break;
+
+			case sense.MIDDLE_SENSE:
+				pub_x = state.x_velo * middle_sense_coeff;
+				pub_y = state.y_velo * middle_sense_coeff;
+				pub_spin = state.spin * spin_middle_sense_coeff;
+				break;
+
+			case sense.LOW_SENSE:
+				pub_x = state.x_velo * low_sense_coeff;
+				pub_y = state.y_velo * low_sense_coeff;
+				pub_spin = state.spin * spin_low_sense_coeff;
+				break;
+			
+			default:
+				break;
+			}
+
+			if(is_update_value){
+				uint8_t angle = state.shot_angle;
+				my_msgs::can_msg msg;
+				msg.id = 0x22;
+				msg.data[7] = angle;
+
+				can_tx.publish(msg);
+			}
+
+			uint8_t msg1_uint8[8], msg2_uint8[8];
+
+			convert_32_8(float_to_int(pub_x), &(msg1_uint8[0]));
+			convert_32_8(float_to_int(pub_y), &(msg1_uint8[4]));
+			convert_32_8(float_to_int(pub_spin), &(msg2_uint8[0]));
+
+			my_msgs::can_msg msg;
+			msg.id = 0x10;
+			for(auto i = 0lu; i < strlen((char*)msg1_uint8); i++){
+				msg.data[i] = msg1_uint8[i];
+			}
+			can_tx.publish(msg);
+
+			msg.id = 0x11;
+			for(auto i = 0lu; i < strlen((char*)msg2_uint8); i++){
+				msg.data[i] = msg2_uint8[i];
+			}
+			can_tx.publish(msg);
+		}
 		//TODO
-		
+
+		if(is_emergency){
+			my_msgs::can_msg msg;
+			msg.id = 0x0f;
+
+			for(auto i = 1; i < 7; i++){
+				msg.id += 0x10;
+				can_tx.publish(msg);
+			}
+		}
+#if TEST
+
+#endif //TEST
+
+		ros::spinOnce();
 		rate.sleep();
 	}
 }
+
+#if TEST
+
+#endif //TEST
+
+//helper functions
+uint32_t float_to_int(float f){
+	uint32_t*p = reinterpret_cast<uint32_t*>(&f);
+	return *p;
+}
+
+
+void convert_32_8(uint32_t i32, uint8_t buf[]){
+	for(int i = 0LU; i < 4; i++){
+		buf[i] = i32>>8*(3-i);
+	}
+}
+
