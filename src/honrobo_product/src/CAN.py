455a3c682250638cd5a1306a7425579d59d778f7
naga <toritonsuibu0527@gmail.com>
2022-09-15 00:20:04 +0900

    operation check was done CAN.py

--- src/honrobo_product/src/CAN.py
+++ src/honrobo_product/src/CAN.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
 import rospy
-from my_msgs.msg import custom
+from my_msgs.msg import can_msg
 import can
 import os
 
-can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan_ctypes')
+try:	
+	os.system('sudo ip link set can0 type can bitrate 500000')
+	os.system('sudo ifconfig can0 up')
+	can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan_ctypes')
+except OSError:
+	rospy.loginfo('error OS')
+	exit()
 
 def callback(msg):
 	can_tx_message = can.Message(arbitration_id=msg.id, data=msg.data, extended_id=False)
@@ -13,26 +19,22 @@ def callback(msg):
 	
 def param_init():
 	rospy.init_node('CAN', anonymous=True)
-	os.system('sudo ip link set can0 type can bitrate 500000')
-	os.system('sudo ifconfig can0 up')
 
 def main():
 	param_init()
-	pub = rospy.Publisher('CAN_RX', custom, queue_size=8)
-	sub = rospy.Subscriber('CAN_TX', custom, callback)
+	pub = rospy.Publisher('CAN_RX', can_msg, queue_size=8)
+	sub = rospy.Subscriber('CAN_TX', can_msg, callback)
 
 	while not rospy.is_shutdown():
-		CAN_msg = can0.recv(0.05)
-		if CAN_msg is not None:
-			msg = custom()
-
-			for n in range(8):
-				msg.data[n] = CAN_msg[n]
+		CAN_msg = can0.recv(0.01)
+		if CAN_msg is not None and 0x00 <= CAN_msg.id <= 0x0f:
+			msg = can_msg()
+			msg.data = CAN_msg.data.copy()
 			msg.id = CAN_msg.arbitration_id
 			pub.publish(msg)
 
 
-if __name__ == '__main__':
-	try:
-		main()
-	except rospy.ROSInterruptException: pass
+
+try:
+	main()
+except rospy.ROSInterruptException: pass
